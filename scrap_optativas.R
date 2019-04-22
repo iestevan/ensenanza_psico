6a603603137bb0cef787f1a0000b050c95471a23
iestevan <ignacioestevan@gmail.com>
2019-04-22 19:40:37 -0300

    Optativas_docentesSIFP
    Scrap de las fichas de las optativas de los docentes usando enlace.SIFP.docente

--- scrap_optativas.R
+++ scrap_optativas.R
@@ -18,18 +18,18 @@ library(splitstackshape)
 year = c("2018")
 ciclo = c("7","8")
 rm(optativas_listado)
-
 for (i in year){
   for (j in ciclo){
     optativas <- read_html(paste0("https://sifp.psico.edu.uy/guias-seminarios-optativos-publicas?field_nombre_guia_value=&field_nombre_pdocente_value=&field_apellido_pdocente_value=&field_modulo_guia_value=All&field_ciclo_guia_value=",j,"&field_nombre_pinstituto_value=&field_cod_materia_guia_value=&field_codigo_grupo_rel_guia_value=&field_anio_value%5Bvalue%5D%5Byear%5D=",i,""))
     nodo_op <- html_node(optativas, xpath = '/html/body/div[1]/div/div[1]/div[3]/div/section/div/div/div/div/div[2]/table')
     optativas <- html_table(nodo_op)
+
     # optativas$Ano = i
     optativas$ciclo = j
     
     enlaces <- data.frame(html_attr(html_nodes(nodo_op, "a"), "href"))
-    names(enlaces) = c("enlace")
-    optativas$enlace = enlaces$enlace
+    names(enlaces) = c("enlace.SIFP.optativas")
+    optativas$enlace.SIFP.optativas = enlaces$enlace.SIFP.optativas
     
     if(exists("optativas_listado")){
       optativas_listado <- rbind(optativas_listado, optativas)
@@ -37,78 +37,67 @@ for (i in year){
   }
 }
 rm(year, ciclo, i, j, optativas, nodo_op, enlaces)
-optativas_listado = optativas_listado %>% 
-  select (Nid, Ano, Cupos, `Codigo de la materia`, ciclo, enlace) 
+optativas_listado = optativas_listado %>%
+  select (Año, Cupos, 'Docente Responsable', 'Código de la materia', 'Código de horario', ciclo, enlace.SIFP.optativas) %>% 
+  mutate(`Docente Responsable` = str_remove (`Docente Responsable`, regex(",", ignore_case = TRUE)))
+  
+rep = data.frame(origen = c("á", "é", "í", "ó", "ú", "ñ"), destino = c("a", "e", "i", "o", "u", "n"))
+for (i in 1:nrow(rep)){
+  origen = rep[i, "origen"]
+  destino = rep[i, "destino"]
+  names(optativas_listado) <- gsub(x = names(optativas_listado), pattern = paste(origen), replacement = paste(destino))
+}
+rm(rep, i, origen, destino)
 
 
-# Obtengo informacion de docentes y creditos
+# Obtengo informacion de docentes y creditos----
 rm(optativas_docentes)
-
-for(i in optativas_listado$enlace){
-  web=tryCatch({read_html(i)},
-               error=function(e) NA)
+for(i in optativas_listado$enlace.SIFP.optativas){
+  web=read_html(i)
   
-  texto = as.character(web)
-  texto_ok = str_replace_all(texto, "[\r\n]" , "")
-  
+  lista = as.data.frame(i)
   
   # # Obtengo creditos por optativa
+  texto = as.character(web)
+  texto_ok = str_replace_all(texto, "[\r\n]" , "")
   get_creditos = 'Créditos:.</div><div class=\\\"field-items\\\"><div class=\\\"field-item even\\\">([^</div]*)'
   creditos = str_match(texto_ok, get_creditos)
   
-  creditos = creditos[2]
-  
-  if(exists("creditos_optativas")){
-    creditos_optativas <- rbind(creditos_optativas, creditos)
-  } else {creditos_optativas<- creditos}
-  
-  
-  # Obtengo docentes que participan de la optativa
-  largo_pre_nombre = 'Nombre:.</div><div class=\\\"field-items\\\"><div class=\\\"field-item even\\\">'
-  largo_pre_apellido = 'Apellido:.</div><div class=\\\"field-items\\\"><div class=\\\"field-item even\\\">'
-  
-  get_nombre = 'Nombre:.</div><div class=\\\"field-items\\\"><div class=\\\"field-item even\\\">([^</div]*)'
-  get_apellido = 'Apellido:.</div><div class=\\\"field-items\\\"><div class=\\\"field-item even\\\">([^</div]*)'
-  
-  #Chequeo cuantos docentes participan de la optativa
-  cantidad_docentes = str_count(texto_ok, pattern = get_nombre) 
-  
-  keep = data.frame(matrix (ncol=2,nrow=cantidad_docentes))
-  n <- c("Nombre", "Apellido")
-  colnames(keep) <- n
-  
-  for (j in 1:cantidad_docentes){
+  lista$creditos = creditos[2]
+
+  #Obtengo docentes que participan de la optativa
+  enlace.SIFP.docente = web %>% 
+    html_nodes('.profile2-perfil-docente') %>% 
+    html_nodes("a")%>% 
+    html_attr("href") %>% 
     
-    pre_nombre = as.data.frame(str_locate_all(texto_ok, pattern = largo_pre_nombre)) #identifico donde empiezan y terminan las expresiones regular hasta el nombre
-    nombre = as.data.frame(str_locate_all(texto_ok, pattern = get_nombre)) #identifico donde empiezan y terminan las expresiones regular incluyendo el nombre
-    nombre$start=(pre_nombre$end +1) # encuentro la posici??n donde empieza el nombre
-    
-    keep$Nombre[j] = substr(texto_ok,nombre$start[j],nombre$end[j]) #levanto el nombre
-    
-    pre_apellido =as.data.frame( str_locate_all(texto_ok, pattern = largo_pre_apellido))# idem para el nombre
-    apellido = as.data.frame(str_locate_all(texto_ok, pattern = get_apellido))
-    apellido$start=(pre_apellido$end + 1)
-    
-    keep$Apellido[j] = substr(texto_ok,apellido$start[j],apellido$end[j])}  
-  
-  a = paste(keep$Nombre, keep$Apellido, sep=" ")
-  b = paste(a,collapse = ',')
-  
-  docentes = b
+  if (length(enlace.SIFP.docente)>0) {
+    lista = bind_rows(replicate(length(enlace.SIFP.docente), lista, simplify = FALSE))
+    lista$enlace.SIFP.docente = enlace.SIFP.docente
+  } else{lista$enlace.SIFP.docente = NA
+    }
   
   if(exists("optativas_docentes")){
-    optativas_docentes <- rbind(optativas_docentes, docentes)
-  } else {optativas_docentes <- docentes}
+    optativas_docentes <- rbind(optativas_docentes, lista)
+  } else {optativas_docentes <- lista
+    }
 }
+rm(i, web, lista, get_creditos, creditos, texto, texto_ok, enlace.SIFP.docente)
+names(optativas_docentes) = c("enlace.SIFP.optativas", "creditos", "enlace.SIFP.docente")
 
-optativas_docentes =as.data.frame(optativas_docentes)
-names(optativas_docentes) <- c("Docente")
-optativas_docentes=cSplit(optativas_docentes, "Docente", ",")
+#porque en las fichas contienen dos horarios pero que están ingresados más de una vez en el listado y genera duplicacion
+#al contar la cantidad de clases hay que buscar por docente, y de enlace.SIFP.optativas contar la cantidad de veces que aparece en optativas y la cantidad de docentes cada vez
+optativas_docentes = optativas_docentes %>% 
+  distinct(enlace.SIFP.optativas, enlace.SIFP.docente, .keep_all = TRUE)
+  
+#agrego numero de docentes al listado----
 
-creditos_optativas = as.data.frame(creditos_optativas)
-names(creditos_optativas) <- c("Creditos")
+optativas = optativas_docentes %>% 
+  group_by (enlace.SIFP.optativas, creditos) %>% 
+  summarise (Cantidad.docentes = n()) %>% 
+  left_join(optativas_listado, ., by="enlace.SIFP.optativas")
+rm (optativas_listado)
 
-optativas_listado <- bind_cols(optativas_listado, optativas_docentes)
-optativas_listado <- bind_cols(optativas_listado, creditos_optativas)
-
-save(optativas_listado,file="optativas_listado.RData")
+#guardo las salidas----
+save(optativas,file="optativas.RData")
+save(optativas_docentes,file="optativas_docentes.RData")
